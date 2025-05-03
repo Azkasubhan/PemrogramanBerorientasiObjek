d1548274a6c5238d6b595604d424ccae39626c4a
MuhammadAzkaSubhan <159773390+Azkasubhan@users.noreply.github.com>
2025-05-03 20:21:20 +0700

    Update Anabul.java

--- Tugas/Generik/Anabul.java
+++ Tugas/Generik/Anabul.java
@@ -1,16 +1,1 @@
-package Generik;
 
-public abstract class Anabul {
-    protected String nama;
-
-    public Anabul(){
-        this.nama = "-";
-    }
-
-    public Anabul(String nama) {
-        this.nama = nama;
-    }
-
-    public abstract void gerak();
-    public abstract void bersuara();
-}
